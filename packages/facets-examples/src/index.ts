cb2dbf19c1ed98bd23e2fde09fe2c4c40d6e0adc
Dario Segura <darionco.dev@gmail.com>
2019-11-13 14:36:26 -0500

    [EXAMPLES] Added plugins to the environment

--- packages/facets-examples/src/index.ts
+++ packages/facets-examples/src/index.ts
@@ -1,6 +1,9 @@
 import * as Facets from '@uncharted.software/facets-core';
 export {Facets};
 
+import * as Plugins from '@uncharted.software/facets-plugins';
+export {Plugins};
+
 import {bootstrap} from './ExampleBootstrap';
 
 export const Examples = {
