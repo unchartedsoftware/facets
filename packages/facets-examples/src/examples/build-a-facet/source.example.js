45b3cfbbaa34aed7d80f6eeab2a3e84cf608386f
Dario Segura <darionco.dev@gmail.com>
2019-12-05 09:28:49 -0500

    [ESLINT] Pass

--- packages/facets-examples/src/examples/build-a-facet/source.example.js
+++ packages/facets-examples/src/examples/build-a-facet/source.example.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     const facetList = document.querySelector('#example');
     facetList.data = [
         {
@@ -7,7 +7,7 @@ window.addEventListener('load', function() {
                 ratio: 0.456,
                 label: 'Example Facet',
                 value: 456,
-            }
+            },
         },
         {
             type: 'facet-term',
@@ -15,7 +15,7 @@ window.addEventListener('load', function() {
                 ratio: 0.456,
                 label: 'Example Facet',
                 value: 456,
-            }
+            },
         },
         {
             type: 'facet-term',
@@ -29,22 +29,22 @@ window.addEventListener('load', function() {
             type: 'facet-histogram',
             data: {
                 values: [
-                    { ratio: 0.1, labels: { left: 0, right: 1 } },
-                    { ratio: 0.2, labels: { left: 1, right: 2 } },
-                    { ratio: 0.3, labels: { left: 2, right: 3 } },
-                    { ratio: 0.4, labels: { left: 3, right: 4 } },
-                    { ratio: 0.5, labels: { left: 4, right: 5 } },
-                    { ratio: 0.6, labels: { left: 5, right: 6 } },
-                    { ratio: 0.7, labels: { left: 6, right: 7 } },
-                    { ratio: 0.8, labels: { left: 7, right: 8 } },
-                    { ratio: 0.9, labels: { left: 8, right: 9 } },
-                    { ratio: 1.0, labels: { left: 9, right: 10 } }
+                    {ratio: 0.1, labels: {left: 0, right: 1}},
+                    {ratio: 0.2, labels: {left: 1, right: 2}},
+                    {ratio: 0.3, labels: {left: 2, right: 3}},
+                    {ratio: 0.4, labels: {left: 3, right: 4}},
+                    {ratio: 0.5, labels: {left: 4, right: 5}},
+                    {ratio: 0.6, labels: {left: 5, right: 6}},
+                    {ratio: 0.7, labels: {left: 6, right: 7}},
+                    {ratio: 0.8, labels: {left: 7, right: 8}},
+                    {ratio: 0.9, labels: {left: 8, right: 9}},
+                    {ratio: 1.0, labels: {left: 9, right: 10}},
                 ],
-            }
-        }
+            },
+        },
     ];
 
-    setTimeout(function() {
+    setTimeout(function () {
         facetList.data = [
             {
                 type: 'facet-term',
@@ -52,7 +52,7 @@ window.addEventListener('load', function() {
                     ratio: 0.123,
                     label: 'Example Facet',
                     value: 123,
-                }
+                },
             },
             {
                 type: 'facet-term',
@@ -60,7 +60,7 @@ window.addEventListener('load', function() {
                     ratio: 0.789,
                     label: 'Example Facet',
                     value: 789,
-                }
+                },
             },
             {
                 type: 'facet-term',
@@ -74,19 +74,19 @@ window.addEventListener('load', function() {
                 type: 'facet-histogram',
                 data: {
                     values: [
-                      	{ ratio: 1.0, labels: { left: 9, right: 10 } },
-                      	{ ratio: 0.9, labels: { left: 8, right: 9 } },
-                      	{ ratio: 0.8, labels: { left: 7, right: 8 } },
-                      	{ ratio: 0.7, labels: { left: 6, right: 7 } },
-                      	{ ratio: 0.6, labels: { left: 5, right: 6 } },
-                      	{ ratio: 0.5, labels: { left: 4, right: 5 } },
-                      	{ ratio: 0.4, labels: { left: 3, right: 4 } },
-                      	{ ratio: 0.3, labels: { left: 2, right: 3 } },
-                      	{ ratio: 0.2, labels: { left: 1, right: 2 } },
-                        { ratio: 0.1, labels: { left: 0, right: 1 } }
+                        {ratio: 1.0, labels: {left: 9, right: 10}},
+                        {ratio: 0.9, labels: {left: 8, right: 9}},
+                        {ratio: 0.8, labels: {left: 7, right: 8}},
+                        {ratio: 0.7, labels: {left: 6, right: 7}},
+                        {ratio: 0.6, labels: {left: 5, right: 6}},
+                        {ratio: 0.5, labels: {left: 4, right: 5}},
+                        {ratio: 0.4, labels: {left: 3, right: 4}},
+                        {ratio: 0.3, labels: {left: 2, right: 3}},
+                        {ratio: 0.2, labels: {left: 1, right: 2}},
+                        {ratio: 0.1, labels: {left: 0, right: 1}},
                     ],
-                }
-            }
+                },
+            },
         ];
     }, 1000);
 });
