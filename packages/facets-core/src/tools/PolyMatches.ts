839eb3b91f955be1c690ac56a5b09d0abf5af374
Dario Segura <darionco.dev@gmail.com>
2019-12-17 11:50:10 -0500

    [ESLINT] Pass

--- packages/facets-core/src/tools/PolyMatches.ts
+++ packages/facets-core/src/tools/PolyMatches.ts
@@ -3,7 +3,7 @@ const kPolyMatches =
     (Element.prototype as any).msMatchesSelector ||
     Element.prototype.webkitMatchesSelector ||
     ((): void => {
-        throw new Error('Browsers without a native implementation of `Element.prototype.matches` are not supported')
+        throw new Error('Browsers without a native implementation of `Element.prototype.matches` are not supported');
     })();
 
 export function polyMatches(target: Element, selector: string): boolean {
