5d1aa1408a0b9e8b13efef30487efeed249e3d8a
Dario Segura <darionco.dev@gmail.com>
2020-01-30 00:11:38 -0500

    [CORE] Exports timeline + timeline labels + makeIconSVG utility

--- packages/facets-core/src/index.ts
+++ packages/facets-core/src/index.ts
@@ -8,13 +8,16 @@ export {FacetTerms} from './facet-terms/FacetTerms';
 export {FacetTermsValue} from './facet-terms-value/FacetTermsValue';
 
 export {FacetBarsBase} from './facet-bars-base/FacetBarsBase';
+
 export {FacetBars} from './facet-bars/FacetBars';
 export {FacetBarsValue} from './facet-bars-value/FacetBarsValue';
-
-export {FacetTimeline} from './facet-timeline/FacetTimeline';
-
 export {FacetBarsLabels} from './facet-plugin/default/facet-bars-labels/FacetBarsLabels';
 export {FacetBarsSelection} from './facet-plugin/default/facet-bars-selection/FacetBarsSelection';
 
+export {FacetTimeline} from './facet-timeline/FacetTimeline';
+export {FacetTimelineLabels} from './facet-plugin/default/facet-timeline-labels/FacetTimelineLabels';
+
 export {FacetList} from './facet-list/FacetList';
 export {FacetPlugin} from './facet-plugin/FacetPlugin';
+
+export {makeIconSVG} from './tools/faIcons';
