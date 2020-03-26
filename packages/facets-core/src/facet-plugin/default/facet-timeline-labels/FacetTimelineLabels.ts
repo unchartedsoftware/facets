9ff0286f69cd69116547c2c32c5d1743b58ae290
Dario Segura <darionco.dev@gmail.com>
2020-03-25 21:24:32 -0400

    [CORE][TIMELINE] Labels are aware of domains that use floats

--- packages/facets-core/src/facet-plugin/default/facet-timeline-labels/FacetTimelineLabels.ts
+++ packages/facets-core/src/facet-plugin/default/facet-timeline-labels/FacetTimelineLabels.ts
@@ -39,6 +39,7 @@ export class FacetTimelineLabels extends FacetPlugin {
             const labels: TemplateResult[] = [];
             const barArea = facet.barAreaElement;
             if (barArea) {
+                const domain = facet.domain;
                 const view = facet.view;
                 const values = facet.values;
                 const viewLength = view[1] - view[0];
@@ -54,71 +55,73 @@ export class FacetTimelineLabels extends FacetPlugin {
                     const i = v - view[0];
                     let tickHeight = 2;
 
-                    if (value && value.label) {
-                        // get the label for the value
-                        const label = Array.isArray(value.label) ? value.label : [value.label];
+                    if (v >= domain[0] && v <= domain[1]) {
+                        if (value && value.label) {
+                            // get the label for the value
+                            const label = Array.isArray(value.label) ? value.label : [value.label];
 
-                        // if we are not keeping track of all the levels in the label, initialize them
-                        if (labelLevels.length < label.length) {
-                            for (let ii = labelLevels.length, nn = label.length; ii < nn; ++ii) {
-                                labelLevels[ii] = '';
-                            }
-                        }
-
-                        // check which level should be rendered
-                        let renderLevel = -1;
-                        for (let ii = label.length - 1; ii >= 0; --ii) {
-                            if (label[ii] !== labelLevels[ii]) {
-                                renderLevel = ii;
-                                break;
-                            }
-                        }
-
-                        // if a level should be rendered, check if there's enough space to render it
-                        if (renderLevel >= 0 && renderedWidth <= barStep * i) {
-                            const labelWidth = this.computeLabelWidth(label[renderLevel]);
-                            let hasSpace = false;
-                            let offset = 0;
-                            let offsetPercent = 0;
-                            let labelPosition = 0;
-
-                            if (i === 0) {
-                                hasSpace = true;
-                                labelPosition = 0;
-                                offset = padding;
-                                offsetPercent = 0;
-                            } else if (barStep * i + labelWidth * 0.5 > width && renderedWidth <= width - labelWidth - padding) {
-                                hasSpace = true;
-                                labelPosition = width;
-                                offset = labelWidth + padding * 2;
-                                offsetPercent = -100;
-                            } else if (renderedWidth <= barStep * i - labelWidth * 0.5 - padding && renderedWidth + labelWidth + padding < width) {
-                                hasSpace = true;
-                                labelPosition = barStep * i;
-                                offset = labelWidth * 0.5 + padding;
-                                offsetPercent = -50;
+                            // if we are not keeping track of all the levels in the label, initialize them
+                            if (labelLevels.length < label.length) {
+                                for (let ii = labelLevels.length, nn = label.length; ii < nn; ++ii) {
+                                    labelLevels[ii] = '';
+                                }
                             }
 
-                            if (hasSpace) {
-                                tickHeight += renderLevel * 2 + 2;
-                                labelLevels[renderLevel] = label[renderLevel];
-                                renderedWidth = labelPosition + labelWidth + padding * 2 - offset;
+                            // check which level should be rendered
+                            let renderLevel = -1;
+                            for (let ii = label.length - 1; ii >= 0; --ii) {
+                                if (label[ii] !== labelLevels[ii]) {
+                                    renderLevel = ii;
+                                    break;
+                                }
+                            }
 
-                                const positionPercent = (labelPosition / width * 100).toFixed(2);
-                                const labelWeight = renderLevel ? '600' : 'normal';
-                                const labelColor = renderLevel ? '#666768' : '#A7A7A8';
+                            // if a level should be rendered, check if there's enough space to render it
+                            if (renderLevel >= 0 && renderedWidth <= barStep * i) {
+                                const labelWidth = this.computeLabelWidth(label[renderLevel]);
+                                let hasSpace = false;
+                                let offset = 0;
+                                let offsetPercent = 0;
+                                let labelPosition = 0;
 
-                                labels.push(html`
+                                if (i === 0) {
+                                    hasSpace = true;
+                                    labelPosition = 0;
+                                    offset = padding;
+                                    offsetPercent = 0;
+                                } else if (barStep * i + labelWidth * 0.5 > width && renderedWidth <= width - labelWidth - padding) {
+                                    hasSpace = true;
+                                    labelPosition = width;
+                                    offset = labelWidth + padding * 2;
+                                    offsetPercent = -100;
+                                } else if (renderedWidth <= barStep * i - labelWidth * 0.5 - padding && renderedWidth + labelWidth + padding < width) {
+                                    hasSpace = true;
+                                    labelPosition = barStep * i;
+                                    offset = labelWidth * 0.5 + padding;
+                                    offsetPercent = -50;
+                                }
+
+                                if (hasSpace) {
+                                    tickHeight += renderLevel * 2 + 2;
+                                    labelLevels[renderLevel] = label[renderLevel];
+                                    renderedWidth = labelPosition + labelWidth + padding * 2 - offset;
+
+                                    const positionPercent = (labelPosition / width * 100).toFixed(2);
+                                    const labelWeight = renderLevel ? '600' : 'normal';
+                                    const labelColor = renderLevel ? '#666768' : '#A7A7A8';
+
+                                    labels.push(html`
                                 <div
                                 class="facet-timeline-labels-label"
                                 style="top:${tickHeight}px;left:${positionPercent}%;font-weight:${labelWeight};color:${labelColor};transform:translate(${offsetPercent}%,0);">
                                     ${label[renderLevel]}
                                 </div>`);
+                                }
                             }
                         }
-                    }
 
-                    ticks.push(html`<div class="facet-timeline-labels-tick" style="height:${tickHeight}px;left:${(barStepPercentage * i).toFixed(2)}%"></div>`);
+                        ticks.push(html`<div class="facet-timeline-labels-tick" style="height:${tickHeight}px;left:${(barStepPercentage * i).toFixed(2)}%"></div>`);
+                    }
                 }
             }
             return html`
